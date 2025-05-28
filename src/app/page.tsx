5a18d1b5acf1a231ed3d45ba45f896263941c6a1
HeloiseKatharine <katharineheloise@gmail.com>
2025-05-28 20:54:17 -0300

    Desenvolvimento do "Sobre"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Footer from '@/components/layout/footer/footer';
 import Header from '@/components/layout/Header/header';
 import Menu from '@/components/ui/Menu/menu';
+import About from '@/components/layout/About/about';
 
 const Home: React.FC = () => {
   return (
       <div style={{position: 'relative'}}>
         <Header />
         <Menu />
-        <h1>Home!</h1>
+        <About/>
         <Footer />
       </div>
   );
