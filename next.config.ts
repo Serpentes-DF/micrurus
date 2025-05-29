1854ddf527a676d5c6678fc1ba59a015e0434cb2
José Antônio <josefreitas788@gmail.com>
2025-05-29 01:10:57 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output : "export",
     images: {
-        loader: "akamai",
-        path: "/",
+        path: "/micrurus/public",
     },
 };
 
