6c1fc0b0cc11967d10ad01c92cdd18ba4f156955
José Antônio <josefreitas788@gmail.com>
2025-05-29 21:26:32 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
     output: 'export',
+    basePath: isProd ? '/micrurus' : '',
     images: {
         unoptimized: true,
     }
 };
-
 
 export default nextConfig;
