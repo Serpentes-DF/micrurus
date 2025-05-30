79f82fc4ab31c5247d012c9f177d18edb41760d4
José Antônio <josefreitas788@gmail.com>
2025-05-29 21:54:21 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
-const isDev = process.env.NODE_ENV !== 'development';
+const isDev = process.env.NODE_ENV == 'development';
 
 const nextConfig: NextConfig = {
     output: 'export',
-    basePath: isDev ?  '' : '/micrurus',
+    basePath: !isDev ? '/micrurus' : '',
+    assetPrefix: !isDev ? '/micrurus/' : '', // Adicione isso! (com barra no final)
     images: {
         unoptimized: true,
     }
