c8a763deb5f24278ce5213c82656cf4503be2546
José Antônio <josefreitas788@gmail.com>
2025-05-29 22:19:37 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
-    basePath: !isDev ? '/micrurus' : '',
-    assetPrefix: !isDev ? '/micrurus/' : '', // Adicione isso! (com barra no final)
     images: {
         unoptimized: true,
     }
