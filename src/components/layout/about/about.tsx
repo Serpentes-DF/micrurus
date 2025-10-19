c8fb7c319d009fb29fea0f203d75e16f76f38def
josefilo <josefreitas788@gmail.com>
2025-10-19 16:10:31 -0400

    add sobre in homepage

--- src/components/layout/about/about.tsx
+++ src/components/layout/about/about.tsx
@@ -5,26 +5,8 @@ export default function About() {
     <div className={style.Main}>
       <h2 className={style.subTitle}>SOBRE O PROJETO</h2>
       <p className={style.Text}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non
-        sagittis nunc, et condimentum felis. Phasellus convallis, leo et rutrum
-        egestas, arcu neque placerat urna, eget commodo urna justo quis eros.
-        Maecenas consequat ut magna eget lobortis. Cras dapibus nisl ac massa
-        aliquet, a dapibus mi gravida. Orci varius natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus. Nunc ullamcorper lorem
-        enim, eget vestibulum nunc lobortis nec. Cras lobortis neque enim, sit
-        amet mollis nisl ultrices vitae. Nunc lacinia ante id faucibus
-        venenatis. Maecenas maximus porta ullamcorper. Praesent elit massa,
-        malesuada sit amet velit at, convallis aliquam diam. Integer scelerisque
-        eleifend turpis. Vivamus justo massa, fringilla quis eros sit amet,
-        ultrices vestibulum turpis. In dolor quam, tempus nec dui vel, varius
-        aliquet orci. Nam augue purus, hendrerit vel convallis at, facilisis
-        vitae sem. Mauris vehicula elementum nisi, ac sollicitudin quam tempus
-        ut. Maecenas lobortis sapien rutrum mattis lobortis. Nulla libero erat,
-        aliquam iaculis tincidunt quis, tempus et dolor. Maecenas consequat,
-        lorem id dapibus tempor, quam quam blandit tortor, vel elementum risus
-        lacus nec nunc. Aenean suscipit purus sit amet magna mollis pretium. In
-        non semper nisi. Maecenas sagittis euismod consequat. In tincidunt, est
-        ac cursus consequat, urna lacus commodo.
+O projeto Serpentes do Distrito Federal é uma atividade de ciência cidadã, que tem como objetivo monitorar as Serpentes do DF através de registros da comunidade. Assim, buscamos novas ocorrências de espécies e outras variáveis ecológicas, como abundância e sazonalidade, para construir estratégias de conservação para esses animais. O projeto também visa melhorar a convivência dos moradores do Distrito Federal com as serpentes que vivem em nosso quadradinho através da educação ambiental.
+Este projeto faz parte da Coleção Zoológica da Universidade Católica de Brasília em parceria com o Laboratório de Fauna e Unidades de Conservação da  Universidade de Brasília.
       </p>
     </div>
   );
